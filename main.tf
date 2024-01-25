643e15645648168f5e40548c4915d1ac47cee59d
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2024-01-25 12:12:07 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "local" {
+  name     = "rg-hashitalks-team-workspace"
+  location = "swedencentral"
+}
+
+module "resource-group-module" {
+  source      = "app.terraform.io/mattias-fjellstrom/resource-group-module/hashitalks"
+  version     = "1.0.1"
+  location    = "westeurope"
+  name_suffix = "hashitalks-resource-group"
+  tags = {
+    team        = "HashiTalks Team"
+    project     = "HashiTalks Project"
+    cost_center = "1234"
+  }
+}
